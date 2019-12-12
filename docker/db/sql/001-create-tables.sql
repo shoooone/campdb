e592cb1458697ed7d229fbd93dcbfc810c64ae35
shoooone <pride.a.s.w.12.01@gmail.com>
2019-12-12 18:48:32 +0900

    drop table

--- docker/db/sql/001-create-tables.sql
+++ docker/db/sql/001-create-tables.sql
@@ -1,8 +1,11 @@
+---- drop ----
+drop table IF exists maker_mst;
+
 ---- create ----
 create table IF not exists maker_mst(
-  maker_id     varchar(10) not null,
+  id           varchar(10) not null,
   name         varchar(20) not null,
   official_url varchar(200),
   image_url    varchar(200),
-  primary key(maker_id)
+  primary key(id)
 ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
